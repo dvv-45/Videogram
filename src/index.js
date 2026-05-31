4a3ac3b4388e0a509ae61d37c8ccf7bd427edabd
dvv-45 <dvishnu8425@gmail.com>
2026-05-31 10:30:25 +0530

    Established Mongo DB connection

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv, { configDotenv } from "dotenv"
+import connectDB from "./db/index.js";
+ 
+dotenv.config({
+    path:'./.env'
+})
+
+
+
+
+connectDB()
+
+// import express from "express"
+// const app = express();
+
+// (async()=>{
+//     try{
+//         await mongooese.connet(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("Error:",error);
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+
+//     }catch(error){
+//         console.log("Error:",error);
+//         throw error;
+//     }
+// })()
