d81e06674f580d8dbe57c9367ac290040335ffb7
dvv-45 <dvishnu8425@gmail.com>
2026-05-31 11:39:58 +0530

    utlis code

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+}).catch((err)=>{
+    console.log("MONGO DB connection failed !!!",error)
+})
 
 // import express from "express"
 // const app = express();
