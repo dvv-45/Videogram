0b6d050818690bdaf06d1f307cb4628f8bd5f297
dvv-45 <dvishnu8425@gmail.com>
2026-06-02 13:10:31 +0530

    fix: update refresh token validation logic and add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -151,7 +151,7 @@ const logoutUser = asyncHandler(async(req,res)=>{
 const refershAccessToken = asyncHandler(async(req,res)=>{
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if(incomingRefreshToken){
+    if(!incomingRefreshToken){
         throw new ApiError(401,"Unauthorized request")
     }
    try {
